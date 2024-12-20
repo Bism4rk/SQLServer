b121b50ec03b82f078baa62c82f2c1aa0a422006
Bismarck <brunowasborn2rock@gmail.com>
2024-12-20 16:26:46 -0300

    BACKUP

---
+++ Aula 67/SQLQuery1.sql
@@ -0,0 +1,9 @@
+SELECT @@VERSION;
+
+-- Como fazer backup de um DB de SQL
+-- 1. clique direito no banco
+-- 2. Tarefas -> Fazer Backup
+-- 3. colocar um nome
+-- 4. Adicionar -> ... -> caminho e nome do arquivo (DEVE CONTER A EXTENSÃO .BAK NELE!!!!!)
+-- 5. OK
+-- 6. feito!
